a07bdbb9077defe9bc5418ccae1d2c35be4b7a79
noelvazquez17 <73671246+noelvazquez17@users.noreply.github.com>
2023-04-22 13:34:51 -0600

    Prueba

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <p>
         &copy; {new Date().getFullYear()} LegalAI. Todos los derechos reservados.
       </p>
-      <Link href="/privacy">Politica de Privacidad</Link>
+      <Link href="/#">Politica de Privacidad</Link>
       <Link href="/terms">Terminos y Condiciones</Link>
       <Link href="/contact">Contacto</Link>
       <Link href="/about">Acerca de LegalAI</Link>
